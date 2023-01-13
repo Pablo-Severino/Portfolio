8a3270fdf67be7ce5af19c05743cc4c169b9bf6b
Pablo-Severino <pabloseverino40@gmail.com>
2023-01-13 02:17:28 -0300

    refactor: redoing the layout

--- components/Footer/IndexFooter.js
+++ components/Footer/IndexFooter.js
@@ -6,20 +6,12 @@ import styles from "../../styles/Footer.module.css";
 export default function IndexFooter() {
   return (
     <footer className={styles.footer}>
-        <a
-          href="https://sakai-ui.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        Powered by{' '}
-        <span className={styles.logo}>
-          <Image src="logo.svg" alt="Vercel Logo" width={120} height={50} />
-          <Image src="Github.svg" alt="Vercel Logo" width={32} height={32} />
-          <Image src="Instagram.svg" alt="Vercel Logo" width={32} height={32} />
-          <Image src="Twitter.svg" alt="Vercel Logo" width={32} height={32} />
-          <Image src="Linkedin.svg" alt="Vercel Logo" width={32} height={32} />
-        </span>
-      </a>
+      <span className={styles.logo}>
+        <Image src="Github.svg" alt="Vercel Logo" width={32} height={32} />
+        <Image src="Instagram.svg" alt="Vercel Logo" width={32} height={32} />
+        <Image src="Twitter.svg" alt="Vercel Logo" width={32} height={32} />
+        <Image src="Linkedin.svg" alt="Vercel Logo" width={32} height={32} />
+      </span>
     </footer>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
