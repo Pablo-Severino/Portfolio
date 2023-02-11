b767b4ac4fa45d5750ca3dcfad714baa8510ac74
Pablo Severino <77620695+Pablo-Severino@users.noreply.github.com>
2023-02-11 08:41:33 -0300

    :white_check_mark: My portfolio done.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
-import IndexNavbar from '@/components/Navbar/IndexNavbar'
-import IndexHeader from '@/components/Header/IndexHeader'
-import IndexFooter from '@/components/Footer/IndexFooter'
+import IndexNavbar from "@/components/Navbar/IndexNavbar";
+import IndexHeader from "@/components/Header/IndexHeader";
+import IndexFooter from "@/components/Footer/IndexFooter";
 
-import stackbitz from '@/public/previews/stackbitz.png'
-import sakai from '@/public/previews/sakai.png'
-import galaxy from '@/public/previews/galaxy.png'
+import stackbitz from "@/public/previews/stackbitz.png";
+import sakai from "@/public/previews/sakai.png";
+import galaxy from "@/public/previews/galaxy.png";
 
-import linkedin from '@/public/linkedin.svg'
-import linkgithub from '@/public/github.svg'
-import link from '@/public/external-link.svg'
+import linkedin from "../public/linkedin.svg";
+import linkgithub from "../public/github.svg";
+import link from "@/public/external-link.svg";
 
-import javascript from '@/public/linguages/javascript.svg'
-import react from '@/public/linguages/react.svg'
-import nextjs from '@/public/linguages/nextjs.svg'
-import mongodb from '@/public/linguages/mongodb.svg'
-import typescript from '@/public/linguages/typescript.svg'
-import tailwindcss from '@/public/linguages/tailwindcss.svg'
-import nodejs from '@/public/linguages/nodejs.svg'
-import git from '@/public/linguages/git.svg'
-import github from '@/public/linguages/github.svg'
-import html5 from '@/public/linguages/html5.svg'
-import css3 from '@/public/linguages/css3.svg'
-import vite from '@/public/linguages/vite.svg'
-import vscode from '@/public/linguages/vscode.svg'
-import figma from '@/public/linguages/figma.svg'
+import javascript from "@/public/linguages/javascript.svg";
+import react from "@/public/linguages/react.svg";
+import nextjs from "@/public/linguages/nextjs.svg";
+import mongodb from "@/public/linguages/mongodb.svg";
+import typescript from "@/public/linguages/typescript.svg";
+import tailwindcss from "@/public/linguages/tailwindcss.svg";
+import nodejs from "@/public/linguages/nodejs.svg";
+import git from "@/public/linguages/git.svg";
+import github from "@/public/linguages/github.svg";
+import html5 from "@/public/linguages/html5.svg";
+import css3 from "@/public/linguages/css3.svg";
+import vite from "@/public/linguages/vite.svg";
+import vscode from "@/public/linguages/vscode.svg";
+import figma from "@/public/linguages/figma.svg";
 
-import illustration from '@/public/dev-ed-wave.svg'
+import illustration from "@/public/dev-ed-wave.svg";
 
-import styles from '@/styles/Linguages.module.css'
-import styled from '@/styles/Home.module.css'
+import styles from "@/styles/Linguages.module.css";
+import styled from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -52,13 +52,21 @@ export default function Home() {
             <div className={styled.preview1}>
               <div className={styled.previewbg1}>
                 <Link href="https://stack-bitz.vercel.app" target="_blank">
-                  <Image src={stackbitz} alt="project demo" className={styled.stackbitz} />
+                  <Image
+                    src={stackbitz}
+                    alt="project demo"
+                    className={styled.stackbitz}
+                  />
                 </Link>
               </div>
             </div>
             <div className={styled.description1}>
               <h1>StackBitz UI</h1>
-              <p>StackBitz is a website of UI components, created with Tailwind CSS, Utilizing React, JavaScript, Next.js, Tailwind CSS, Firebase</p>
+              <p>
+                StackBitz is a website of UI components, created with Tailwind
+                CSS, Utilizing React, JavaScript, Next.js, Tailwind CSS,
+                Firebase
+              </p>
               <div className={styled.tecnolog1}>
                 <div className={styled.space1}>
                   <span className={styles.bxreact}>
@@ -86,11 +94,22 @@ export default function Home() {
                   </span>
                 </div>
               </div>
-              <Link href="https://github.com/Pablo-Severino/StackBitz-UI" target="_blank">
-                <Image src={linkgithub} alt="logotipo github" className={styled.github1} />
+              <Link
+                href="https://github.com/Pablo-Severino/StackBitz-UI"
+                target="_blank"
+              >
+                <Image
+                  src={linkgithub}
+                  alt="logotipo github"
+                  className={styled.github1}
+                />
               </Link>
               <Link href="https://stack-bitz.vercel.app" target="_blank">
-                <Image src={link} alt="logotipo link" className={styled.link1} />
+                <Image
+                  src={link}
+                  alt="logotipo link"
+                  className={styled.link1}
+                />
               </Link>
             </div>
           </div>
@@ -98,13 +117,20 @@ export default function Home() {
             <div className={styled.preview2}>
               <div className={styled.previewbg2}>
                 <Link href="https://sakai-ui.vercel.app">
-                  <Image src={sakai} alt="project demo" className={styled.sakai} />
+                  <Image
+                    src={sakai}
+                    alt="project demo"
+                    className={styled.sakai}
+                  />
                 </Link>
               </div>
             </div>
             <div className={styled.description2}>
               <h1>Sakai UI</h1>
-              <p>Sakai UI is a theming website, using React, TypeScript, Next.js, Firebase, JWT</p>
+              <p>
+                Sakai UI is a theming website, using React, TypeScript, Next.js,
+                Firebase, JWT
+              </p>
               <div className={styled.tecnolog1}>
                 <div className={styled.space7}>
                   <span className={styles.bxreact}>
@@ -132,11 +158,22 @@ export default function Home() {
                   </span>
                 </div>
               </div>
-              <Link href="https://github.com/Pablo-Severino/Sakai-UI" target="_blank">
-                <Image src={linkgithub} alt="logotipo github" className={styled.github2} />
+              <Link
+                href="https://github.com/Pablo-Severino/Sakai-UI"
+                target="_blank"
+              >
+                <Image
+                  src={linkgithub}
+                  alt="logotipo github"
+                  className={styled.github2}
+                />
               </Link>
               <Link href="https://sakai-ui.vercel.app" target="_blank">
-                <Image src={link} alt="logotipo link" className={styled.link2} />
+                <Image
+                  src={link}
+                  alt="logotipo link"
+                  className={styled.link2}
+                />
               </Link>
             </div>
           </div>
@@ -144,13 +181,20 @@ export default function Home() {
             <div className={styled.preview1}>
               <div className={styled.previewbg1}>
                 <Link href="https://galaxy-films.vercel.app">
-                  <Image src={galaxy} alt="project demo" className={styled.galaxy} />
+                  <Image
+                    src={galaxy}
+                    alt="project demo"
+                    className={styled.galaxy}
+                  />
                 </Link>
               </div>
             </div>
             <div className={styled.description3}>
               <h1>Galaxy Films</h1>
-              <p>Galaxy Films is a NetFlix clone, using React, JavaScript, Next.js, Firebase, React Icons</p>
+              <p>
+                Galaxy Films is a NetFlix clone, using React, JavaScript,
+                Next.js, Firebase, React Icons
+              </p>
               <div className={styled.tecnolog1}>
                 <div className={styled.space1}>
                   <span className={styles.bxreact}>
@@ -173,11 +217,22 @@ export default function Home() {
                   </span>
                 </div>
               </div>
-              <Link href="https://github.com/Pablo-Severino/Galaxy-Films" target="_blank">
-                <Image src={linkgithub} alt="logotipo github" className={styled.github1} />
+              <Link
+                href="https://github.com/Pablo-Severino/Galaxy-Films"
+                target="_blank"
+              >
+                <Image
+                  src={linkgithub}
+                  alt="logotipo github"
+                  className={styled.github1}
+                />
               </Link>
               <Link href="https://galaxy-films.vercel.app" target="_blank">
-                <Image src={link} alt="logotipo link" className={styled.link1} />
+                <Image
+                  src={link}
+                  alt="logotipo link"
+                  className={styled.link1}
+                />
               </Link>
             </div>
           </div>
@@ -186,11 +241,19 @@ export default function Home() {
           <h2>Skills & Tools</h2>
           <div className={styled.tools}>
             <div className={styled.javascript}>
-              <Image src={javascript} alt="logo javascript" className={styled.js} />
+              <Image
+                src={javascript}
+                alt="logo javascript"
+                className={styled.js}
+              />
               <p>Javascript</p>
             </div>
             <div className={styled.react}>
-              <Image src={react} alt="logo reactjs" className={styled.reactjs} />
+              <Image
+                src={react}
+                alt="logo reactjs"
+                className={styled.reactjs}
+              />
               <p>React JS</p>
             </div>
             <div className={styled.nextjs}>
@@ -198,15 +261,27 @@ export default function Home() {
               <p>Next JS</p>
             </div>
             <div className={styled.mongodb}>
-              <Image src={mongodb} alt="logo mongodb" className={styled.mongo} />
+              <Image
+                src={mongodb}
+                alt="logo mongodb"
+                className={styled.mongo}
+              />
               <p>MongoDB</p>
             </div>
             <div className={styled.typescript}>
-              <Image src={typescript} alt="logo typescript" className={styled.ts} />
+              <Image
+                src={typescript}
+                alt="logo typescript"
+                className={styled.ts}
+              />
               <p>Typescript</p>
             </div>
             <div className={styled.tailwindcss}>
-              <Image src={tailwindcss} alt="logo tailwindcss" className={styled.tailwind} />
+              <Image
+                src={tailwindcss}
+                alt="logo tailwindcss"
+                className={styled.tailwind}
+              />
               <p>Tailwind CSS</p>
             </div>
             <div className={styled.nodejs}>
@@ -218,7 +293,11 @@ export default function Home() {
               <p>GIT</p>
             </div>
             <div className={styled.github}>
-              <Image src={github} alt="logo github" className={styled.githubimg} />
+              <Image
+                src={github}
+                alt="logo github"
+                className={styled.githubimg}
+              />
               <p>GitHub</p>
             </div>
             <div className={styled.html5}>
@@ -234,7 +313,11 @@ export default function Home() {
               <p>Vite</p>
             </div>
             <div className={styled.vscode}>
-              <Image src={vscode} alt="logo vscode" className={styled.vscodeimg} />
+              <Image
+                src={vscode}
+                alt="logo vscode"
+                className={styled.vscodeimg}
+              />
               <p>VSCode</p>
             </div>
             <div className={styled.figma}>
@@ -253,13 +336,23 @@ export default function Home() {
             <h4>São Paulo, Brazil</h4>
           </div>
           <div className={styled.contact}>
-            <Link href="https://mailto:pabloseverino40@gmail.com" target="_blank">
+            <Link
+              href="https://mailto:pabloseverino40@gmail.com"
+              target="_blank"
+            >
               <h4>pabloseverino40@gmail.com</h4>
             </Link>
-            <Link href="https://www.linkedin.com/in/pablo-severino-b23883207" target="_blank">
+            <Link
+              href="https://www.linkedin.com/in/pablo-severino-b23883207"
+              target="_blank"
+            >
               <div className={styled.space13}>
                 <span>
-                  <Image src={linkedin} alt="logo linkedin" className={styled.linkedinlogo} />
+                  <Image
+                    src={linkedin}
+                    alt="logo linkedin"
+                    className={styled.linkedinlogo}
+                  />
                   <h5 className={styled.txtlinkedin}>Linkedin</h5>
                 </span>
               </div>
@@ -267,16 +360,24 @@ export default function Home() {
             <Link href="https://github.com/Pablo-Severino" target="_blank">
               <div className={styled.space14}>
                 <span>
-                  <Image src={linkgithub} alt="logo github" className={styled.githublogo} />
+                  <Image
+                    src={linkgithub}
+                    alt="logo github"
+                    className={styled.githublogo}
+                  />
                   <h5 className={styled.txtgithub}>Github</h5>
                 </span>
               </div>
             </Link>
           </div>
-          <Image src={illustration} alt="illustration" className={styled.illustration} />
+          <Image
+            src={illustration}
+            alt="illustration"
+            className={styled.illustration}
+          />
         </div>
       </main>
       <IndexFooter />
     </>
-  )
+  );
 }
